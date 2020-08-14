27f2d056d04214c4f0af8e489e9931d346878c42
fryingpannn <matthewdukepan@gmail.com>
2020-08-14 19:09:49 -0400

    jawsDB

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,10 +1,17 @@
 <?php
 //setting up database
 
-$servername = "us-cdbr-east-02.cleardb.com"; //localhost:8889
-$dBUsername = "bafee8afa994bd";	//root
-$dBPassword = "4dc6fca9"; //root or ""
-$dbName = "heroku_01fdeda2cc1aa3c"; //was trendymarkt
+//remote connection (jawsDB)
+$servername = "z37udk8g6jiaqcbx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dBUsername = "bgg6h8p8t8tr9d8x";	
+$dBPassword = "grf6g3l4bbmayisf"; 
+$dbName = "rykppv8n7h82a40v";
+
+//local connection
+// $servername = "localhost:8889";
+// $dBUsername = "root";
+// $dBPassword = "root"; // or ""
+// $dbName = "trendymarkt";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dbName);
 
