d37ca2a0ff653aa60dfac6336c5117d7860608a4
fryingpannn <matthewdukepan@gmail.com>
2020-08-14 16:06:36 -0400

    database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 
 $servername = "us-cdbr-east-02.cleardb.com"; //localhost:8889
 $dBUsername = "bafee8afa994bd";	//root
-$dBPassword = "4dc6fca"; //root or ""
+$dBPassword = "4dc6fca9"; //root or ""
 $dbName = "heroku_01fdeda2cc1aa3c"; //was trendymarkt
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dbName);
@@ -11,3 +11,5 @@ $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dbName);
 if(!$conn) {
     die("Connection failed: ".mysqli_connect_error());
 }
+
+mysql://bafee8afa994bd:4dc6fca9@us-cdbr-east-02.cleardb.com/heroku_01fdeda2cc1aa3c?reconnect=true
