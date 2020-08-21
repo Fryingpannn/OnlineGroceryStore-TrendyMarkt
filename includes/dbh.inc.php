acb70916b5dd1e473fc2f147261bc095916ebb7a
fryingpannn <matthewdukepan@gmail.com>
2020-08-20 22:17:12 -0400

    local change

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,16 +2,16 @@
 //setting up database
 
 //remote connection env variables (jawsDB): DEPLOY WITH THIS
-// $servername = getenv("MYSQL_HOST");
-// $dBUsername = getenv("MYSQL_USER");	
-// $dBPassword = getenv("MYSQL_PASS"); 
-// $dbName = getenv("MYSQL_DB");
+$servername = getenv("MYSQL_HOST");
+$dBUsername = getenv("MYSQL_USER");	
+$dBPassword = getenv("MYSQL_PASS"); 
+$dbName = getenv("MYSQL_DB");
 
 //local connection: DONT DEPLOY WITH THIS
-$servername = "localhost:8889";
-$dBUsername = "root";
-$dBPassword = "root"; // or ""
-$dbName = "trendymarkt";
+// $servername = "localhost:8889";
+// $dBUsername = "root";
+// $dBPassword = "root"; // or ""
+// $dbName = "trendymarkt";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dbName);
 
