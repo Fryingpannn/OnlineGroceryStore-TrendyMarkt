3bf4977ebff3b3096d39296d0096db0daffea342
fryingpannn <matthewdukepan@gmail.com>
2020-08-20 20:22:50 -0400

    db non local

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,17 +1,17 @@
 <?php
 //setting up database
 
-//remote connection (jawsDB): DEPLOY WITH THIS
-// $servername = getenv("MYSQL_HOST");
-// $dBUsername = getenv("MYSQL_USER");	
-// $dBPassword = getenv("MYSQL_PASS"); 
-// $dbName = getenv("MYSQL_DB");
+//remote connection env variables (jawsDB): DEPLOY WITH THIS
+$servername = getenv("MYSQL_HOST");
+$dBUsername = getenv("MYSQL_USER");	
+$dBPassword = getenv("MYSQL_PASS"); 
+$dbName = getenv("MYSQL_DB");
 
 //local connection: DONT DEPLOY WITH THIS
-$servername = "localhost:8889";
-$dBUsername = "root";
-$dBPassword = "root"; // or ""
-$dbName = "trendymarkt";
+// $servername = "localhost:8889";
+// $dBUsername = "root";
+// $dBPassword = "root"; // or ""
+// $dbName = "trendymarkt";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dbName);
 
